3eb61606359df02d3b11ca8c609133eb6a6fd2ad
Jose Lazaro Conceição de Jesus <jose.lazaro@educacao.ba.gov.br>
2020-01-15 20:01:07 -0300

    mais estudos javascript

--- evento.js
+++
@@ -1,7 +0,0 @@
-let carros = ["palio 98", "toro", "uno"];
-
-carros.forEach(function(value, index){
-
-    console.log(index, value);
-
-});
---
+++ foreach.js
@@ -0,0 +1,15 @@
+/*let carros = ["palio 98", "toro", "uno"];
+
+carros.forEach(function(value, index){
+
+    console.log(index, value);
+
+});*/
+
+
+let avioes = ["Gol", "Avianc", "Vasp", "Latam"];
+
+avioes.forEach(function(value, index){
+
+    console.log(index, value);
+})
